89ad0474ad43371a5baf7a0defcc75c3762885ac
where.liu <where.liu@huolala.cn>
2022-06-23 10:41:22 +0800

    feat: init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,15 @@
+export default {
+  title: "Code Style Guide",
+  description: "Code Style Best Practices",
+  themeConfig: {
+    nav: [{ text: "JS", link: "/js/" }],
+    sidebar: {
+      "/js/": [
+        {
+          text: "JavaScirpt",
+          items: [{ text: "Prettier", link: "/js/prettier" }],
+        },
+      ],
+    },
+  },
+};
