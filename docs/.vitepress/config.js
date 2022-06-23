b428635d0fecb5a6a7c0cbb7592c5285ca1d5203
Where <liuzhaowei55@gmail.com>
2022-06-23 23:41:16 +0800

    feat: remove CNAME

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,6 +4,11 @@ export default {
   lang: 'zh-CN',
   logo: 'logo.png',
   themeConfig: {
+    algolia: {
+      appId: 'J0TJ9SD1IK',
+      apiKey: '6118ce2dd52f087a80b2065af6374040',
+      indexName: 'style_fanfou_dev',
+    },
     nav: [
       { text: 'JavaScript', link: '/js/' },
       { text: 'PHP', link: '/php/' },
