9c75a6b8d68a681c03f26441c2cc58dd8730b74b
where.liu <where.liu@huolala.cn>
2022-06-23 19:49:27 +0800

    feat: robots

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,24 +1,25 @@
 export default {
-  title: "Code Style Guide",
-  description: "Code Style Best Practices",
-  lang: "zh-CN",
+  title: 'Code Style Guide',
+  description: 'Code Style Best Practices',
+  lang: 'zh-CN',
+  logo: 'logo.png',
   themeConfig: {
     nav: [
-      { text: "JavaScritp", link: "/js/" },
-      { text: "PHP", link: "/php/" },
-      { text: "Java", link: "/java/" },
+      { text: 'JavaScritp', link: '/js/' },
+      { text: 'PHP', link: '/php/' },
+      { text: 'Java', link: '/java/' },
     ],
     sidebar: {
-      "/js/": [
+      '/js/': [
         {
-          text: "JavaScirpt",
+          text: 'JavaScirpt',
           items: [
-            { text: "husky", link: "/js/husky" },
-            { text: "commitlint", link: "/js/commitlint" },
-            { text: "lint-staged", link: "/js/lint-staged" },
-            { text: "Prettier", link: "/js/prettier" },
-            { text: "ESLint", link: "/js/eslint" },
-            { text: "Stylelint", link: "/js/stylelint" },
+            { text: 'husky', link: '/js/husky' },
+            { text: 'commitlint', link: '/js/commitlint' },
+            { text: 'lint-staged', link: '/js/lint-staged' },
+            { text: 'Prettier', link: '/js/prettier' },
+            { text: 'ESLint', link: '/js/eslint' },
+            { text: 'Stylelint', link: '/js/stylelint' },
           ],
         },
       ],
