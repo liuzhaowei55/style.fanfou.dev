147d13cf1fadfde7f08cb79a3bc4342ec45d113b
where.liu <where.liu@huolala.cn>
2022-06-27 17:17:28 +0800

    feat: editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,8 +36,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/liuzhaowei55/style.fanfou.dev/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
