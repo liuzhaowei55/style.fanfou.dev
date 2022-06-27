0757319a0a771cfbeeb4cec817745a0de8044b94
where.liu <where.liu@huolala.cn>
2022-06-27 16:54:00 +0800

    feat: docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -74,6 +74,16 @@ const config = {
         darkTheme: darkCodeTheme,
       },
     }),
+  themes: [
+    [
+      require.resolve('@easyops-cn/docusaurus-search-local'),
+      {
+        hashed: true,
+        indexBlog: false,
+        language: ['zh'],
+      },
+    ],
+  ],
 };
 
 module.exports = config;
