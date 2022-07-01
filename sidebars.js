470983b11168ecd85162ef69f8b4eeeb5edf5700
where.liu <where.liu@huolala.cn>
2022-07-01 11:20:49 +0800

    feat: java

--- sidebars.js
+++ sidebars.js
@@ -2,7 +2,6 @@
 
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
-  // By default, Docusaurus generates a sidebar from the docs folder structure
   docs: [
     { type: 'doc', id: 'index' },
     {
